260e15ba780fe8a18bb8713465559ef3828c30f0
hank880 <153570550+hank880@users.noreply.github.com>
2025-04-15 13:09:57 +0800

    Add files via upload

---
+++ story/script.js
@@ -0,0 +1,6 @@
+// Toggle Navigation Menu for Mobile Devices
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+  }
+  
